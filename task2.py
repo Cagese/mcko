a5fb5aa64195dad9dd5e50ecb006b489b09e76e6
mikha <mikhail_morozov@mail.ru>
2024-02-03 15:50:05 +0300

    task2 commit

---
+++ task2.py
@@ -0,0 +1,18 @@
+import csv
+with open('scientist.txt', encoding='utf8') as file:
+    # Открываем файл scientist.txt и с помощью модуля csv парсим его
+    file = list(csv.reader(file, delimiter='#', quotechar='"'))
+    # сортировка по дате
+    for i in range(len(file)):
+        j = i - 1
+        key = file[i]
+        while file[j][2] > key[2] and j >= 0:
+            file[j + 1] = file[j]
+            j -= 1
+        file[j + 1] = key
+
+for i in range(1,6):
+    # Вывод нужных данных в консоль
+    print(f'{file[i][0]}:{file[i][1]}')
+    print(file[i][2])
+
